b6c9015c9d159c8cc21db2131d02ca26cbbbed29
dengchenglin <297854263@qq.com>
2015-12-31 09:12:48 +0800

    Initial Commit

---
+++ CLLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLLabel
+//
+//  Created by chenguangjiang on 15/12/31.
+//  Copyright © 2015年 dengchenglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CLLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CLLabel
+//
+//  Created by chenguangjiang on 15/12/31.
+//  Copyright © 2015年 dengchenglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
